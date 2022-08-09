95021b41dc545246f0d80cb25a5897390eb9c249
Estugon <estugon-tem@gmx.de>
2022-08-09 14:59:46 +0200

    Added some comments

--- starter/custom_logic.cpp
+++ starter/custom_logic.cpp
@@ -1,14 +1,27 @@
 #include "logic.h"
 
+/**
+ * @brief A subclass of a game logic.
+ *        Here you can implement your own logic, which will pick the moves in the game.
+ * 
+ */
 class RandomLogic : public Logic::Logic
 {
 private:
 public:
     RandomLogic() : Logic::Logic() { }
     
+    /**
+     * @brief This method will be called once the server requests a move.
+     * 
+     * @param gameState The current state of the game. 
+     * @return Game::Move 
+     */
     Game::Move getMove(Game::GameState gameState) {
+        // Get all moves that are currently possible.
         std::vector<Game::Move> possibleMoves = gameState.getPossibleMoves();
 
+        // Return the chosen move, in this case the first move in the vector.
         return possibleMoves[0];
     }
 };
--- starter/starter.cpp
+++ starter/starter.cpp
@@ -3,6 +3,7 @@
 #include "custom_logic.cpp"
 
 int main(int argc, char *argv[]) {
+    // Choose your custom logic here
     Logic::Logic logic = RandomLogic();
     Client::Game_Client client(logic);
     client.Start(argc, argv);
