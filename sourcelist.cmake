90839c08d2eaaa93f583f7ab0b70df2d5eef1f1c
Estugon <estugon-tem@gmx.de>
2022-08-09 10:44:53 +0200

    Updated the sourcelist

--- sourcelist.cmake
+++ sourcelist.cmake
@@ -1,6 +1,9 @@
 set(sources ${sources}
     src/game/coordinates.cpp
     src/game/board.cpp
+    src/game/result.cpp
+    src/game/gameState.cpp
+    src/game/move.cpp
     src/client/client.cpp
     src/communication/message_handler.cpp
     src/communication/tcp_connection.cpp
