2a31ff3af874f8d87ffa160d50224f479bf331ce
Estugon <estugon-tem@gmx.de>
2022-08-16 15:32:26 +0200

    Removed some lines that only existed for testing

--- starter/custom_logic.cpp
+++ starter/custom_logic.cpp
@@ -25,29 +25,6 @@ public:
             std::cout << "ERROR: NO MOVES FOUND!" << std::endl;
             throw;
         }
-
-        /// TESTING AREA
-        std::cout << "====================================================\n";
-        std::cout <<    "OLD GameState Test\n" << 
-                        "Turn: " << gameState.get_turn() << 
-                        "\nCurrent Team: " << ((gameState.current_team) ? "TWO" : "ONE") <<
-                        "\nFishCount ONE=" << gameState.get_fishes_one() <<
-                        "\nPenguins Set ONE=" << gameState.get_penguins_one() <<
-                        "\nFishCount TWO=" << gameState.get_fishes_two() <<
-                        "\nPenguins Set TWO=" << gameState.get_penguins_two() << std::endl; 
-        std::cout << "OLD Board:\n" << gameState.get_board().to_string();
-
-        std::cout << "====================================================\n";
-        
-        gameState.perform_move(possibleMoves[0]);
-        std::cout <<    "NEW GameState Test\n" << 
-                        "Turn: " << gameState.get_turn() << 
-                        "\nCurrent Team: " << ((gameState.current_team) ? "TWO" : "ONE") <<
-                        "\nFishCount ONE=" << gameState.get_fishes_one() <<
-                        "\nPenguins Set ONE=" << gameState.get_penguins_one() <<
-                        "\nFishCount TWO=" << gameState.get_fishes_two() <<
-                        "\nPenguins Set TWO=" << gameState.get_penguins_two() << std::endl; 
-        std::cout << "NEW Board:\n" << gameState.get_board().to_string();
         // Return the chosen move, in this case the first move in the vector.
         return possibleMoves[0];
     }
